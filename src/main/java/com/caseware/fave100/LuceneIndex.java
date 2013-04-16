2d9b2eb5e071de0945a74340f55b8950415a420b
Yissachar Radcliffe <yissachar.radcliffe@caseware.com>
2013-04-16 10:12:43 -0400

    Switched to WhitespaceAnalyzer so that $ and _ will be correctly searched

--- src/main/java/com/caseware/fave100/LuceneIndex.java
+++ src/main/java/com/caseware/fave100/LuceneIndex.java
@@ -3,8 +3,7 @@ package com.caseware.fave100;
 import java.io.File;
 import java.io.IOException;
 
-import org.apache.lucene.analysis.standard.StandardAnalyzer;
-import org.apache.lucene.analysis.util.CharArraySet;
+import org.apache.lucene.analysis.core.WhitespaceAnalyzer;
 import org.apache.lucene.index.DirectoryReader;
 import org.apache.lucene.index.IndexReader;
 import org.apache.lucene.search.IndexSearcher;
@@ -13,12 +12,12 @@ import org.apache.lucene.store.FSDirectory;
 import org.apache.lucene.util.Version;
 
 public class LuceneIndex {
-	public static StandardAnalyzer ANALYZER;
+	public static WhitespaceAnalyzer ANALYZER;
 	public static IndexSearcher SEARCHER;
 	public static final Version LUCENE_VERSION = Version.LUCENE_42;
 
 	static {
-		ANALYZER = new StandardAnalyzer(LUCENE_VERSION, new CharArraySet(LUCENE_VERSION, 0, true));
+		ANALYZER = new WhitespaceAnalyzer(LUCENE_VERSION);
 
 		final File file = new File(Thread.currentThread().getContextClassLoader().getResource("lucene-index").getPath());
 
