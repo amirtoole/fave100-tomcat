e81f1597f7391b6ae8b1a8d3023e380fb41ae694
Yissachar Radcliffe <yissachar.radcliffe@caseware.com>
2013-07-29 13:48:43 -0400

    Finished upgrade to Lucene 4.4

--- src/main/java/org/musicbrainz/search/LuceneVersion.java
+++ src/main/java/org/musicbrainz/search/LuceneVersion.java
@@ -1,6 +1,7 @@
 package org.musicbrainz.search;
+
 import org.apache.lucene.util.Version;
 
 public class LuceneVersion {
-	public static final Version LUCENE_VERSION = Version.LUCENE_43;
+	public static final Version LUCENE_VERSION = Version.LUCENE_44;
 }
--- src/main/java/org/musicbrainz/search/analysis/LuceneVersion.java
+++
@@ -1,6 +0,0 @@
-package org.musicbrainz.search.analysis;
-import org.apache.lucene.util.Version;
-
-public class LuceneVersion {
-	public static final Version LUCENE_VERSION = Version.LUCENE_43;
-}
